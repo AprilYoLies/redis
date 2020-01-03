6d5a9bf7421484b152736ce64ba14260525817e3
AprilYoLies <863821569@qq.com>
2020-01-03 13:54:59 +0800

    初始化工程

---
+++ CMakeFiles/redis-cli.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/eva/CLionProjects/redis/src/adlist.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/adlist.c.o"
+  "/Users/eva/CLionProjects/redis/src/ae.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/ae.c.o"
+  "/Users/eva/CLionProjects/redis/src/anet.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/anet.c.o"
+  "/Users/eva/CLionProjects/redis/src/crc16.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/crc16.c.o"
+  "/Users/eva/CLionProjects/redis/src/crc64.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/crc64.c.o"
+  "/Users/eva/CLionProjects/redis/src/dict.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/dict.c.o"
+  "/Users/eva/CLionProjects/redis/src/redis-cli.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/redis-cli.c.o"
+  "/Users/eva/CLionProjects/redis/src/release.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/release.c.o"
+  "/Users/eva/CLionProjects/redis/src/sds.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/sds.c.o"
+  "/Users/eva/CLionProjects/redis/src/siphash.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/siphash.c.o"
+  "/Users/eva/CLionProjects/redis/src/zmalloc.c" "/Users/eva/CLionProjects/redis/CMakeFiles/redis-cli.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "deps/linenoise"
+  "deps/hiredis"
+  "deps/lua/src"
+  "deps/hiredis/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/eva/CLionProjects/redis/deps/linenoise/CMakeFiles/linenoise.dir/DependInfo.cmake"
+  "/Users/eva/CLionProjects/redis/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
